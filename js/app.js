7fc983eb596576ee9fb1e4d74ffbf01ec118e981
Jennifer Nguyen <nguyenj1203@gmail.com>
2016-08-07 07:30:57 -0700

    first v1

---
+++ js/app.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+	                            
+	$('.ryu').mouseenter(function() {
+	$('.ryu-still').hide();
+	$('.ryu-cool').hide();
+	$('.ryu-ready').show();
+
+	})
+		.mouseleave(function() {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+			$('.ryu-ready').hide();
+	
+	})
+		.mousedown(function(){
+		//play hadouken sound
+		playHadouken();
+		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$('.hadouken').hide();
+				$('.hadouken').css('left', '520');
+			});
+		//animate hadouken to the right of the screen
+	})
+
+		.mouseup(function(){
+		$('.ryu-throwing').hide();
+		$('ryu-cool').hide();
+		$('.ryu-ready').show();
+	})
+
+		$(document).keydown(function(e) {
+			if (e.keyCode === 88) {
+				$('.ryu-still').hide();
+				$('.ryu-ready').hide();
+				$('.ryu-cool').show();
+			};
+
+		})
+			
+		.keyup(function(e) {
+			if (e.keyCode === 88) {
+				$('.ryu-still').show();
+				$('.ryu-ready').hide();
+				$('.ryu-cool').hide();
+			};
+
+		});
+});
+
+function playHadouken () {
+
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
